71e71cff2b947333ede5d6dad7d46c3c4de0cfd7
ylnnyu <ly783@nyu.edu>
2015-08-09 13:48:11 -0700

    fix bug

--- EmpowerDemo/ScrollDetailViewController.h
+++ EmpowerDemo/ScrollDetailViewController.h
@@ -7,14 +7,17 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "ARSegmentPageController.h"
 
 @interface ScrollDetailViewController : UIViewController <UIScrollViewDelegate>
 {
     UIPageControl *pageControl;
     BOOL pageControlBingUsed;
+
 }
 @property(nonatomic) CGPoint contentOffset;
 @property (nonatomic,retain) UIPageControl *pageControl;
 @property (nonatomic,retain) UIView *chartView;
 @property (nonatomic,retain) UIScrollView *scrollView;
+@property(nonatomic,retain) ARSegmentPageController *pager;
 @end
